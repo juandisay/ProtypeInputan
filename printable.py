28fc349e8d25ffc86146e6c22cf569b151e5dc0e
juandisay <juandi.syafrin@gmail.com>
2015-12-31 05:24:20 +0700

    new release dec

--- printable.py
+++ printable.py
@@ -1,10 +1,4 @@
 #!/usr/bin/env python
 
 class result:
-    def __init__(self,name,address,phone):
-        self.name = name
-        self.address = address
-        self.phone = phone
-        self.cetak()
-    def cetak (self):
-        pass
+    pass
